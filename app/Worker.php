5a924de6f32695daeb0e35dc9c288f81b8dc2ab5
NoorMAAC <maria_02_angeles@hotmail.com>
2020-09-04 02:08:32 -0500

    Cambio en Worker

--- app/Worker.php
+++ app/Worker.php
@@ -8,6 +8,6 @@ class Worker extends Model
 {
     protected $table='workers';
     protected $primaryKey='codeWorker';
-    protected $fillable=['nameWorker','lastNameWorker','dniWorker','addressWorker','civilStatus','telephone','socialSecurity','flatWorker','dateWorker'];
+    protected $fillable=['nameWorker','lastNameWorker','dniWorker','addressWorker','civilStatus','telephone','socialSecurity','flatWorker','dateWorker','statusWorker'];
     public $timestamps=false;
 }
